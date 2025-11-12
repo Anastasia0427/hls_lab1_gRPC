3e5d81661bbb3bd4f6858e635584c3a9eabaad0e
Anastasia <167350995+Anastasia0427@users.noreply.github.com>
2025-11-12 22:38:45 +0300

    add files (client + server + proto)

---
+++ gRPCServer/Server.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using lab1_grpc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5001, o => o.Protocols = HttpProtocols.Http2);
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+
+app.Run();
+
+public class GreeterService : Greeter.GreeterBase
+{
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Msg = "Hello " + request.Name
+        });
+    }
+}
